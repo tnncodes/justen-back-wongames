b71316a479417174889c71caed0d18836bac97e5
theandersonn <theandersonn@gmail.com>
2021-01-28 10:18:10 -0300

    criando dados com service.create

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -6,6 +6,7 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
@@ -29,6 +30,16 @@ module.exports = {
     const {
       data: { products },
     } = await axios.get(gopApiUrl);
+
+    await strapi.services.publisher.create({
+      name: products[0].publisher,
+      slug: slugify(products[0].publisher).toLowerCase(),
+    });
+
+    await strapi.services.developer.create({
+      name: products[0].developer,
+      slug: slugify(products[0].developer).toLowerCase(),
+    });
 
     console.log(await getGameInfo(products[0].slug));
   },
