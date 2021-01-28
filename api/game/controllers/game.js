b11c083b2d07e58ae3e66df4877fbe67ec420c12
theandersonn <theandersonn@gmail.com>
2021-01-27 22:16:59 -0300

    criando rota e controller para o populate

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Starting to populate...");
+    ctx.send({ ok: true });
+  },
+};
